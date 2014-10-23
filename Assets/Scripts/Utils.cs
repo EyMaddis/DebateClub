90b2dfe3ba62c67d27fbad9a9f3a62c3930469cf
Mathis Neumann <github@madd.is>
2014-10-23 23:51:40 +0300

    Refactored functions to Utils.cs. Additional commentary for the camera script

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Utils {
+
+    public static float LimitValue(float value, float min , float max)
+    {
+        value = Math.Max(value, min);
+        value = Math.Min(value, max);
+        return value;
+    }
+
+    public static Vector2 DeleteZDimension(Vector3 vec)
+    {
+        return new Vector2(vec.x, vec.y);
+    }
+}
