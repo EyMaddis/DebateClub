a1deed5df99c5f5f1bbe8c4696806edf864dc8f1
Mathis Neumann <github@madd.is>
2014-10-25 14:57:54 +0300

    fix inconsistent zooming when the offset is too big Ingore /Library/*

--- Assets/Scripts/TwoPlayerCamera2D.cs
+++ Assets/Scripts/TwoPlayerCamera2D.cs
@@ -16,13 +16,14 @@ public class TwoPlayerCamera2D : MonoBehaviour
     public Vector2 LowerBoundary = new Vector2(-10f, -10f);
     public Vector2 UpperBoundary = new Vector2(10f, 10f);
     
-    [Tooltip("Offset of the players to the screen boundaries")]
-    public float Offset = 5f;
 
 
     [Header("Automatic Zoom Controls")]
     [Range(0.1f, 10.0f)]
-    public float minSizeY = 1f;
+    public float MinSizeY = 1f;
+
+    [Tooltip("Offset of the players to the screen boundaries")]
+    public Vector2 Offset = new Vector2(1f, -2f);
 
     [Range(0.001f, 1.0f)] 
     public float ZoomMultiplierX = 0.3f;
@@ -69,15 +70,28 @@ public class TwoPlayerCamera2D : MonoBehaviour
     {
         //horizontal size is based on actual screen ratio
         var aspectRatio = Screen.width/Screen.height;
-        var minSizeX = minSizeY * aspectRatio;
+        var minSizeX = MinSizeY * aspectRatio;
 
         //multiplying by 0.5, because the ortographicSize is actually half the height
-        var width = Mathf.Abs(Target1.position.x - Target2.position.x - Offset) * ZoomMultiplierX;
-        var height = Mathf.Abs(Target1.position.y - Target2.position.y - Offset) * ZoomMultiplierY;
+        var xDelta = Target1.position.x - Target2.position.x;
+        var width = Mathf.Abs(Mathf.Max(xDelta - Offset.x, xDelta)) * ZoomMultiplierX;
+
+        var xDeltaReversed = Target2.position.x - Target1.position.x;
+        var width2 = Mathf.Abs(Mathf.Max(xDeltaReversed - Offset.x, xDeltaReversed)) * ZoomMultiplierX;
+
+
+        var yDelta = Target2.position.y - Target1.position.y;
+        var yDeltaReversed = Target1.position.y - Target2.position.y;
+
+        var height = Mathf.Abs(Mathf.Max(yDelta - Offset.y, yDelta)) * ZoomMultiplierY;
+        var height2 = Mathf.Abs(Mathf.Max(yDeltaReversed - Offset.y, yDeltaReversed)) * ZoomMultiplierY;
+        
+        height = Mathf.Max(height, height2);
+        width = Mathf.Max(width, width2);
 
         //computing the size
         var camSizeX = Mathf.Max(width, minSizeX);
-        var size = Mathf.Max(height, camSizeX, minSizeY);
+        var size = Mathf.Max(height, camSizeX, MinSizeY);
         _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, size, ref _sizeVelocity, SmoothZoomTime);
     }
 }
