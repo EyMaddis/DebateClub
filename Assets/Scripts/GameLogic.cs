fb40540aabb0cc314fc2d27b72e81a12d0130e47
Mathis Neumann <github@madd.is>
2014-11-27 21:18:09 +0200

    Winner. Relational points

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class GameLogic : MonoBehaviour
 {
 
+    public int WinningDifference = 2;
     private Points _points;
+    public Font Font;
 
     void Start()
     {
         _points = GetComponent<Points>();
+        GUI.color = Color.white;
     }
 
     void OnGUI()
@@ -23,12 +26,46 @@ public class GameLogic : MonoBehaviour
             _points.Reset();
         }
         #endif
+
+        int winner = CheckWinCondition();
+        if (winner == 0) return;
+
+
+        var centeredStyle = new GUIStyle
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontStyle = FontStyle.Bold,
+            font = Font,
+            fontSize = 50,
+            normal = {textColor = winner == 1 ? Color.blue : Color.red}
+        };
+
+        GUI.Label(new Rect(0f, 0f, Screen.width, Screen.height), "Player " + winner + " won!", centeredStyle);
     }
+
 
     public void EndRound()
     {
         //GUI.Label(new Rect(150f, 150f, 10f, 10f), "New Round!");
         Application.LoadLevel(Application.loadedLevelName);
         _points.Reset();
+    }
+
+
+    /// <summary>
+    ///  Check if a player has won
+    /// </summary>
+    /// <returns>0 if nobody wins, 1 if player 1 wins, 2 for player 2</returns>
+    public int CheckWinCondition()
+    {
+        var diff = _points.GetPoints(1) - _points.GetPoints(2);
+        if (diff <= -WinningDifference) return 2;
+        return diff >= WinningDifference ? 1 : 0;
+    }
+
+
+    void DrawWinner()
+    {
+
     }
 }
--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Points : MonoBehaviour {
 
 	private static readonly int[] PlayerPoints = {0,0};
-    public int WinningDifference = 2;
 
     public Font Font;
 
@@ -15,53 +14,40 @@ public class Points : MonoBehaviour {
 	    PlayerPoints[playerID+-1] += points;
 	}
 
-    /// <summary>
-    ///  Check if a player has won
-    /// </summary>
-    /// <returns>0 if nobody wins, 1 if player 1 wins, 2 for player 2</returns>
-    public int CheckWinCondition()
-    {
-        var diff = PlayerPoints[0] - PlayerPoints[1];
-        if (diff <= -WinningDifference) return 2;
-        return diff >= WinningDifference ? 1 : 0;
-    }
 
 
 	//Display on screen
 	void OnGUI()
 	{
 	    DrawPoints();
-	    DrawWinner();
 	}
 
     void DrawPoints()
     {
-        var centeredStyle = GUI.skin.GetStyle("Label");
+        var centeredStyle = new GUIStyle();
         centeredStyle.alignment = TextAnchor.UpperCenter;
         centeredStyle.fontStyle = FontStyle.Bold;
         centeredStyle.font = Font;
+        centeredStyle.fontSize = 20;
+        centeredStyle.richText = true;
         var xCenter = (float)Screen.width / 2;
 
 
+        //var oldColor = GUI.color;
         GUILayout.BeginArea(new Rect(xCenter - 50, 15, 100, 50));
         GUILayout.BeginHorizontal(centeredStyle);
 
-        GUI.color = Color.blue;
-        GUILayout.Label("" + GetPoints(1));
+        GUILayout.Label("<color=blue>" + (GetPoints(1)-GetPoints(2))+"</color>");
 
         GUILayout.FlexibleSpace();
 
-        GUI.color = Color.red;
-        GUILayout.Label("" + GetPoints(2));
+        GUILayout.Label("<color=red>" + (GetPoints(2) - GetPoints(1)) + "</color>");
 
         GUILayout.EndHorizontal();
         GUILayout.EndArea();
+        //GUI.color = oldColor;
     }
 
-    void DrawWinner()
-    {
-        
-    }
 
     public int GetPoints(int playerId)
     {
