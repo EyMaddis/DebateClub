b66a8c1adc78ca6cfea503b4e6ea641180853b5b
Mathis Neumann <github@madd.is>
2014-12-07 17:01:02 +0200

    intro animation

---
+++ Assets/Scripts/IntroAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroAnimation : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animationController;
+   // private bool done = false;
+	// Use this for initialization
+	void Start ()
+	{
+	    _spriteRenderer = player.GetComponent<SpriteRenderer>();
+	    _animationController = player.GetComponent<Animator>();
+	    _animationController.enabled = false;
+        _spriteRenderer.enabled = false;
+	}
+
+    void Awake()
+    {
+        _spriteRenderer = player.GetComponent<SpriteRenderer>();
+        _spriteRenderer.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if(!done) _spriteRenderer.enabled = false;
+	}
+
+    public void ShowPlayer()
+    {
+        _spriteRenderer.enabled = true;
+        _animationController.enabled = true;
+        Debug.Log(player.name);
+        //done = true;
+    }
+}
